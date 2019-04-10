6203b00754b54fbf36f261dfe382208a19e36ea5
Benjamin Peters <benjamin.peters@posteo.de>
2019-04-10 18:58:46 -0400

    staticvae: added variational component to autoencoder

--- src/staticvae/models.py
+++ src/staticvae/models.py
@@ -25,7 +25,20 @@ def kaiming_init(m):
             m.bias.data.fill_(0)
 
 
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    """ 
+        from https://github.com/pytorch/examples/blob/master/vae/main.py
+    """
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
 
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return BCE + KLD
 
 class VAE(nn.Module):
     """ encoder/decoder from Higgins for VAE (Chairs, 3DFaces) - image size 64x64x1
@@ -35,9 +48,11 @@ class VAE(nn.Module):
 
     """
 
-
-    def __init__(self, n_latents 10, img_channels = 1):
+    def __init__(self, n_latents = 10, img_channels = 1):
         super(VAE, self).__init__()
+        
+        self.n_latents = n_latents
+        self.img_channels = img_channels
 
         # encoder
         self.conv1 = nn.Conv2d(in_channels = img_channels, out_channels = 32, kernel_size = 4, stride = 2, padding = 0)     # B, 32, 31, 31
@@ -45,10 +60,11 @@ class VAE(nn.Module):
         self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 4, stride = 2, padding = 0)               # B, 64, 6, 6
         self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 4, stride = 2, padding = 0)               # B, 64, 2, 2
 
-        self.fc_enc = nn.Linear(256, n_latent*2, bias = True)
+        self.fc_enc_mu = nn.Linear(256, n_latents, bias = True)
+        self.fc_enc_logvar = nn.Linear(256, n_latents, bias = True)
 
         # decoder
-        self.fc = nn.Linear(n_latent*2, 256, bias = True)                         # B, 256 (after .view(): B, 64, 2, 2)
+        self.fc_dec = nn.Linear(n_latents, 256, bias = True)                         # B, 256 (after .view(): B, 64, 2, 2)
 
         self.convT4 = nn.ConvTranspose2d(64, 64, 4, 2, 0)                       # B, 64, 6, 6
         self.convT3 = nn.ConvTranspose2d(64, 32, 4, 2, 0)                       # B, 32, 14, 14
@@ -61,18 +77,32 @@ class VAE(nn.Module):
         for m in self._modules:
             kaiming_init(m)
 
-    def forward(self, x):
+    def reparametrize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
 
+    def encode(self, x):
         x = torch.relu(self.conv1(x))
         x = torch.relu(self.conv2(x))
         x = torch.relu(self.conv3(x))
         x = torch.relu(self.conv4(x))        
-        x = self.fc(x.view(-1, 256))
+        mu = self.fc_enc_mu(x.view(-1, 256))
+        logvar = self.fc_enc_logvar(x.view(-1, 256))
+        return mu, logvar
 
-        x = self.fc(x).view(-1,64,2,2)
+    def decode(self, z):
+        
+        x = self.fc_dec(z).view(-1,64,2,2)
         x = torch.relu(self.convT4(x))
         x = torch.relu(self.convT3(x))
         x = torch.relu(self.convT2(x))
-        x = torch.relu(self.convT1(x))
-
+        x = torch.relu(self.convT1(x)) # maybe use sigmoid instead here?
         return x
+    
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparametrize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+
