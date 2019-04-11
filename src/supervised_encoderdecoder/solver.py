d6c90ffe3b8c0b759480f07ec6e00a79b8f71a3d
Benjamin Peters <benjamin.peters@posteo.de>
2019-04-10 22:10:56 -0400

    solver.py: cleanup, commenting, added running loss to display in terminal, solved pickle bug for trainstats

--- src/supervised_encoderdecoder/solver.py
+++ src/supervised_encoderdecoder/solver.py
@@ -9,14 +9,12 @@ Created on Tue Apr  9 13:22:30 2019
 
 import torch
 from torch.nn import MSELoss
-from torch.optim import Adam, RMSprop
+from torch.optim import RMSprop
 
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import os
 import pickle
-
-import warnings
 
 import sys
 sys.path.append("..") # Adds higher directory to python modules path.
@@ -28,19 +26,23 @@ from models.decoders import decoderBVAE_like, decoderBVAE_like_wElu
 
 class DataGather(object):
     """ 
+        contains a dict that is updated with training related information
+        
+        filename: for checkpointing 
+        
+        might need different/new keys depending on trained model
+    
         modified from
         from https://github.com/1Konny/Beta-VAE/blob/master/solver.py
     """
     def __init__(self, filename):
         
-        self.filename = filename
+        self.filename = '{}.pkl'.format(filename)
         
-        if not os.path.exists(self.filename):
-            self.data = self.get_empty_data_dict()
+        if os.path.isfile(self.filename):
+            self.load_data_dict()
         else:
-            f = open('{}.pkl'.format(filename),"rb")
-            self.data = pickle.load(f)
-            f.close()
+            self.data = self.get_empty_data_dict()            
 
     def get_empty_data_dict(self):
         return dict(iter=[],
@@ -54,8 +56,13 @@ class DataGather(object):
                     reconstructed=[],)
 
     def save_data_dict(self):
-        f = open('{}.pkl'.format(self.filename),"wb")
+        f = open(self.filename,"wb")
         pickle.dump(self.data,f)
+        f.close()
+
+    def load_data_dict(self):
+        f = open(self.filename,"rb")
+        self.data = pickle.load(f)
         f.close()
 
     def insert(self, **kwargs):
@@ -72,13 +79,13 @@ class Solver(object):
         
         self.use_cuda = args.cuda and torch.cuda.is_available()
         self.device = torch.device("cuda:0" if self.use_cuda else "cpu")
-
         
-        self.max_iter = args.max_iter
-        self.global_iter = 0
+        """ 
+            /begin of non-generic part that needs to be modified for each new model
+        """
         
+        # model name is used for checkpointing (and here for setting self.net)
         self.model = args.model
-        self.lr = args.lr
         
         dataloaderparams = {'batch_size': args.batch_size,
                             'shuffle': args.shuffle,
@@ -90,7 +97,6 @@ class Solver(object):
         elif args.dataset.lower() == 'dsprites':
             self.train_loader = torch.utils.data.DataLoader(dSpriteBackgroundDataset(transform=Rescale(32),
                                             shapetype = 'dsprite'), **dataloaderparams)
-        
         
         
         if args.model.lower() == "encoderbvae_like":
@@ -107,9 +113,18 @@ class Solver(object):
             
         
         self.net = net(n_latent = args.n_latent, img_channels = args.img_channels).to(self.device)
+        self.loss = MSELoss()
+
+        self.lr = args.lr
         self.optim = RMSprop(self.net.parameters(), lr=self.lr)
         
-        self.loss = MSELoss()
+        
+        """ 
+            /end of non-generic part that needs to be modified for each new model
+        """       
+                
+        self.max_iter = args.max_iter
+        self.global_iter = 0
         
         # prepare checkpointing
         if not os.path.isdir(args.ckpt_dir):
@@ -141,9 +156,9 @@ class Solver(object):
         pbar = tqdm(total=self.max_iter)
         pbar.update(self.global_iter)
         
-        out = False
-        
-        running_loss = 0.0
+        out = False        
+        running_loss_trainstats = 0.0 # running loss for the trainstats (gathered and pickeled)
+        running_loss_terminal_display = 0.0 # running loss for the trainstats (gathered and pickeled)
         
         while not out:
             for samples in self.train_loader: # not sure how long the train_loader spits out data (possibly infinite?)
@@ -153,6 +168,8 @@ class Solver(object):
                 
                 self.net.zero_grad()
                 
+                """ /begin of non-generic part (might need to be adapted for different models / data)"""
+                
                 # get current batch and push to device
                 # ([:, None, :, :] currently because channels are not existent yet in the Dataset output)
                 img_batch, code_batch = samples['image'][:,None,:,:].float().to(self.device), samples['latents'].float().to(self.device)
@@ -160,6 +177,7 @@ class Solver(object):
                 # scale the coordinates such that both the circle and the gaussian center have the same scale
                 code_batch[:, 2:] = code_batch[:,2:] /  32
                 
+                # depending on encoder/decoder image/label are input/target (or vice versa)
                 if self.modeltype == 'encoder':
                     input_batch = img_batch
                     output_batch = code_batch
@@ -173,17 +191,23 @@ class Solver(object):
                 actLoss.backward()
                 self.optim.step()                
 
-                running_loss += actLoss.item()
+                running_loss_trainstats += actLoss.item()
+                running_loss_terminal_display += actLoss.item()
 
+                # update gather object with training information
                 if self.global_iter % self.trainstats_gather_step == 0:
-                    running_loss = running_loss / self.trainstats_gather_step
+                    running_loss_trainstats = running_loss_trainstats / self.trainstats_gather_step
                     self.gather.insert(iter=self.global_iter,
-                                       recon_loss=running_loss,
+                                       recon_loss=running_loss_trainstats,
                                        target = output_batch[0].detach().cpu().numpy(),
                                        reconstructed = predicted_batch[0].detach().cpu().numpy(),)
+                    running_loss_trainstats = 0.0
+                
+                """ /end of non-generic part"""
 
                 if self.global_iter % self.display_step == 0:
-                    pbar.write('iter:{}, loss:{:.3e}'.format(self.global_iter, actLoss))
+                    pbar.write('iter:{}, loss:{:.3e}'.format(self.global_iter, running_loss_terminal_display / self.display_step))
+                    running_loss_terminal_display = 0.0
 
                 if self.global_iter % self.save_step == 0:
                     self.save_checkpoint(self.ckpt_name)
@@ -197,12 +221,11 @@ class Solver(object):
         pbar.write("[Training Finished]")
         pbar.close()
 
-    """
-        checkpointing from:
-        https://github.com/1Konny/Beta-VAE/blob/master/solver.py
-    """                    
     
-    def save_checkpoint(self, filename, silent=True):
+    def save_checkpoint(self, filename, silent=True): 
+        """ saves model and optimizer state as checkpoint 
+            modified from https://github.com/1Konny/Beta-VAE/blob/master/solver.py                   
+        """
         model_states = {'net':self.net.state_dict(),}
         optim_states = {'optim':self.optim.state_dict(),}
 
@@ -218,6 +241,9 @@ class Solver(object):
             print("=> saved checkpoint '{}' (iter {})".format(file_path, self.global_iter))
     
     def load_checkpoint(self, filename):
+        """ loads model and optimizer state from checkpoint 
+            modified from https://github.com/1Konny/Beta-VAE/blob/master/solver.py                   
+        """        
         file_path = os.path.join(self.ckpt_dir, filename)
         if os.path.isfile(file_path):
             checkpoint = torch.load(file_path)
@@ -228,20 +254,3 @@ class Solver(object):
         else:
             print("=> no checkpoint found at '{}'".format(file_path))
 
-    def visualizeReconstruction(self, image, reconstruction):
-        """ so far only copied from initial train.py
-        
-            needs to be modified (e.g., integrated with visdom)
-            
-        """
-        
-        plt.subplot(1,2,1)
-        plt.imshow(image[0,:,:,:].detach().cpu().numpy().squeeze())
-        plt.title('true')
-            
-        plt.subplot(1,2,2)
-        plt.imshow(reconstruction[0,:,:,:].detach().cpu().numpy().squeeze())
-        plt.title('reconstructed')
-            
-        plt.pause(.05)
-        plt.show()
