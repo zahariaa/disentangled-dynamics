b5284fd2b5b1db2f070442dccbec82d23c1d120a
Andrew Zaharia <az2522@columbia.edu>
2019-05-24 17:30:00 -0400

    copied bVAE files to dynamic_bVAE

---
+++ src/dynamic_bVAE/models.py
@@ -0,0 +1,240 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+bVAE (Higgings, 2017) or inspired by the paper
+
+"""
+
+
+
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+
+
+def kaiming_init(m):
+    """ from https://github.com/1Konny/Beta-VAE/blob/master/model.py """
+    if isinstance(m, (nn.Linear, nn.Conv2d)):
+        init.kaiming_normal(m.weight)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+    elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
+        m.weight.data.fill_(1)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+
+
+def reconstruction_loss(x, x_recon, distribution='gaussian'):
+    """
+     from https://github.com/1Konny/Beta-VAE/blob/master/solver.py
+     
+    """
+    
+    batch_size = x.size(0)
+    assert batch_size != 0
+    
+    if distribution == 'bernoulli':
+        recon_loss = nn.functional.binary_cross_entropy(x_recon, x, reduction='sum').div(batch_size)
+    elif distribution == 'gaussian':
+        #x_recon = nn.functional.sigmoid(x_recon)
+        recon_loss = nn.functional.mse_loss(x_recon, x, reduction='sum').div(batch_size)
+    else:
+        recon_loss = None
+        
+    return recon_loss    
+
+def kl_divergence(mu, logvar):
+    """
+     from https://github.com/1Konny/Beta-VAE/blob/master/solver.py
+     
+    """    
+    batch_size = mu.size(0)
+    assert batch_size != 0
+    if mu.data.ndimension() == 4:
+        mu = mu.view(mu.size(0), mu.size(1))
+    if logvar.data.ndimension() == 4:
+        logvar = logvar.view(logvar.size(0), logvar.size(1))
+
+    klds = -0.5*(1 + logvar - mu.pow(2) - logvar.exp())
+    total_kld = klds.sum(1).mean(0, keepdim=True)
+    dimension_wise_kld = klds.mean(0)
+    mean_kld = klds.mean(1).mean(0, keepdim=True)
+    
+    return total_kld, dimension_wise_kld, mean_kld
+
+def loss_function(recon_loss, total_kld, beta = 1):
+    
+    beta_vae_loss = recon_loss + beta*total_kld
+
+    return beta_vae_loss
+
+def beta_from_normalized_beta(beta_normalized, N, M):
+    """
+       input:
+           beta_normalized
+           N: total number of values in each input image (pixels times channels)
+           M: number of latent dimensions
+    
+        computes beta = beta_normalized * N / M
+        
+        given the relationship:
+            
+            \beta_\text{norm} = \frac{\beta M}{N}
+            
+            from the Higgins, 2017, bVAE paper (p. 15)
+    """
+    
+    beta = beta_normalized * N / M
+    return beta
+
+def normalized_beta_from_beta(beta, N, M):
+    """
+       input:
+           beta
+           N: total number of values in each input image (pixels times channels)
+           M: number of latent dimensions
+    
+        computes beta_normalized = beta * latent_code_size / image_size
+        
+        given the relationship:
+            
+            \beta_\text{norm} = \frac{\beta M}{N}
+            
+            from the Higgins, 2017, bVAE paper (p. 15)
+    """
+    
+    beta_normalized = beta * M / N
+    return beta_normalized
+
+class staticVAE64(nn.Module):
+    """ encoder/decoder from Higgins for VAE (Chairs, 3DFaces) - image size 64x64x1
+        from Table 1 in Higgins et al., 2017, ICLR
+
+        number of latents can be adapted, spatial input dimensions are fixed
+
+    """
+
+    def __init__(self, n_latent = 10, img_channels = 1):
+        super(staticVAE64, self).__init__()
+        
+        self.n_latent = n_latent
+        self.img_channels = img_channels
+
+        # encoder
+        self.conv1 = nn.Conv2d(in_channels = img_channels, out_channels = 32, kernel_size = 4, stride = 2, padding = 0)     # B, 32, 31, 31
+        self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 4, stride = 2, padding = 0)               # B, 32, 14, 14
+        self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 4, stride = 2, padding = 0)               # B, 64, 6, 6
+        self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 4, stride = 2, padding = 0)               # B, 64, 2, 2
+
+        self.fc_enc_mu = nn.Linear(256, n_latent, bias = True)
+        self.fc_enc_logvar = nn.Linear(256, n_latent, bias = True)
+
+        # decoder
+        self.fc_dec = nn.Linear(n_latent, 256, bias = True)                         # B, 256 (after .view(): B, 64, 2, 2)
+
+        self.convT4 = nn.ConvTranspose2d(64, 64, 4, 2, 0)                       # B, 64, 6, 6
+        self.convT3 = nn.ConvTranspose2d(64, 32, 4, 2, 0)                       # B, 32, 14, 14
+        self.convT2 = nn.ConvTranspose2d(32, 32, 4, 2, 0, output_padding = 1)   # B, 32, 31, 31
+        self.convT1 = nn.ConvTranspose2d(32, img_channels, 4, 2, 0)             # B, img_channels, 64, 64
+
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            kaiming_init(m)
+
+    def reparametrize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def encode(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+        x = torch.relu(self.conv4(x))        
+        mu = self.fc_enc_mu(x.view(-1, 256))
+        logvar = self.fc_enc_logvar(x.view(-1, 256))
+        return mu, logvar
+
+    def decode(self, z):
+        
+        x = self.fc_dec(z).view(-1,64,2,2)
+        x = torch.relu(self.convT4(x))
+        x = torch.relu(self.convT3(x))
+        x = torch.relu(self.convT2(x))
+        x = torch.relu(self.convT1(x)) # maybe use sigmoid instead here?
+        return x
+    
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparametrize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+
+
+class staticVAE32(nn.Module):
+    """ encoder/decoder from Higgins for VAE (Chairs, 3DFaces) - image size 32x32x1
+        from Table 1 in Higgins et al., 2017, ICLR
+
+        number of latents can be adapted, spatial input dimensions are fixed
+
+    """
+
+    def __init__(self, n_latent = 10, img_channels = 1):
+        super(staticVAE32, self).__init__()
+        
+        self.n_latent = n_latent
+        self.img_channels = img_channels
+
+        # encoder
+        self.conv1 = nn.Conv2d(in_channels = img_channels, out_channels = 32, kernel_size = 3, stride = 2, padding = 1)     # B, 32, 16, 16
+        self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3, stride = 2, padding = 1)               # B, 32, 8, 8
+        self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 3, stride = 2, padding = 1)               # B, 64, 4, 4
+        self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 2, padding = 1)               # B, 64, 2, 2
+
+        self.fc_enc_mu = nn.Linear(256, n_latent, bias = True)
+        self.fc_enc_logvar = nn.Linear(256, n_latent, bias = True)
+
+        # decoder
+        self.fc_dec = nn.Linear(n_latent, 256, bias = True)                         # B, 256 (after .view(): B, 64, 2, 2)
+
+        self.convT4 = nn.ConvTranspose2d(64, 64, 3, 2, 1, 1)                       # B, 64, 4, 4
+        self.convT3 = nn.ConvTranspose2d(64, 32, 3, 2, 1, 1)                       # B, 32, 8, 8
+        self.convT2 = nn.ConvTranspose2d(32, 32, 3, 2, 1, 1)                       # B, 32, 16, 16
+        self.convT1 = nn.ConvTranspose2d(32, img_channels, 3, 2, 1, 1)             # B, img_channels, 32, 32
+
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            kaiming_init(m)
+
+    def reparametrize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def encode(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+        x = torch.relu(self.conv4(x))        
+        mu = self.fc_enc_mu(x.view(-1, 256))
+        logvar = self.fc_enc_logvar(x.view(-1, 256))
+        return mu, logvar
+
+    def decode(self, z):
+        
+        x = self.fc_dec(z).view(-1,64,2,2)
+        x = torch.nn.functional.elu(self.convT4(x))
+        x = torch.nn.functional.elu(self.convT3(x))
+        x = torch.nn.functional.elu(self.convT2(x))
+        x = torch.nn.functional.sigmoid(self.convT1(x)) # maybe use sigmoid instead here?
+        return x
+    
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparametrize(mu, logvar)
+        return self.decode(z), mu, logvar
---
+++ src/dynamic_bVAE/solver.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 13:22:30 2019
+
+@author: benjamin
+"""
+
+
+import torch
+from torch.optim import RMSprop
+from torchvision import transforms
+
+from tqdm import tqdm
+import os
+import pickle
+
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+
+from data.dspritesb import dSpriteBackgroundDataset
+from models import staticVAE32
+from models import loss_function, reconstruction_loss, kl_divergence
+from models import normalized_beta_from_beta, beta_from_normalized_beta
+
+
+class DataGather(object):
+    """ 
+        contains a dict that is updated with training related information
+        
+        filename: for checkpointing 
+        
+        might need different/new keys depending on trained model
+    
+        modified from
+        from https://github.com/1Konny/Beta-VAE/blob/master/solver.py
+    """
+    def __init__(self, filename):
+        
+        self.filename = '{}.pkl'.format(filename)
+        
+        if os.path.isfile(self.filename):
+            self.load_data_dict()
+        else:
+            self.data = self.get_empty_data_dict()            
+
+    def get_empty_data_dict(self):
+        return dict(iter=[],
+                    total_loss=[],
+                    recon_loss=[],
+                    total_kld=[],
+                    dim_wise_kld=[],
+                    mean_kld=[],
+                    mu=[],
+                    var=[],
+                    target=[],
+                    reconstructed=[],)
+
+    def save_data_dict(self):
+        f = open(self.filename,"wb")
+        pickle.dump(self.data,f)
+        f.close()
+
+    def load_data_dict(self):
+        f = open(self.filename,"rb")
+        self.data = pickle.load(f)
+        f.close()
+
+    def insert(self, **kwargs):
+        for key in kwargs:
+            self.data[key].append(kwargs[key])
+
+    def flush(self):
+        self.data = self.get_empty_data_dict()
+
+
+class Solver(object):
+    
+    def __init__(self, args):
+        
+        self.use_cuda = args.cuda and torch.cuda.is_available()
+        self.device = torch.device("cuda:0" if self.use_cuda else "cpu")
+        
+        """ 
+            /begin of non-generic part that needs to be modified for each new model
+        """
+        
+        # model name is used for checkpointing (and here for setting self.net)
+        self.model = args.model
+        
+        self.image_size = args.image_size
+        self.n_latent = args.n_latent
+        self.img_channels = args.img_channels
+        
+        # beta for beta VAE
+        if args.beta_is_normalized:
+            self.beta_norm = args.beta
+            self.beta = beta_from_normalized_beta(self.beta_norm,N= self.image_size * self.image_size * self.img_channels,M=args.n_latent)
+        else:
+            self.beta = args.beta
+            self.beta_norm = normalized_beta_from_beta(self.beta,N= self.image_size * self.image_size * self.img_channels,M=args.n_latent)            
+        
+        dataloaderparams = {'batch_size': args.batch_size,
+                            'shuffle': args.shuffle,
+                            'num_workers': args.num_workers}
+
+        if args.dataset.lower() == 'dsprites_circle':
+            self.train_loader = torch.utils.data.DataLoader(dSpriteBackgroundDataset(transform=transforms.Resize((self.image_size,self.image_size)),
+                                            shapetype = 'circle'), **dataloaderparams)
+        elif args.dataset.lower() == 'dsprites':
+            self.train_loader = torch.utils.data.DataLoader(dSpriteBackgroundDataset(transform=transforms.Resize((self.image_size,self.image_size)),
+                                            shapetype = 'dsprite'), **dataloaderparams)
+        
+        
+        if args.model.lower() == "staticvae32":
+            net = staticVAE32
+            self.modeltype = 'staticVAE'
+        else:
+            raise Exception('model "%s" unknown' % args.model)
+            
+        
+        self.net = net(n_latent = self.n_latent, img_channels = self.img_channels).to(self.device)
+        
+        self.reconstruction_loss = reconstruction_loss
+        self.kl_divergence = kl_divergence
+        
+        self.loss = loss_function
+
+        self.lr = args.lr
+        self.optim = RMSprop(self.net.parameters(), lr=self.lr)
+        
+        
+        """ 
+            /end of non-generic part that needs to be modified for each new model
+        """       
+                
+        self.max_iter = args.max_iter
+        self.global_iter = 0
+        
+        # prepare checkpointing
+        if not os.path.isdir(args.ckpt_dir):
+            os.mkdir(args.ckpt_dir)
+        
+        self.ckpt_dir = args.ckpt_dir
+        self.load_last_checkpoint = args.load_last_checkpoint
+        self.ckpt_name = '{}_nlatent={}_betanorm={}_{}_last'.format(self.model.lower(), self.n_latent, self.beta_norm, args.dataset.lower())
+        
+        
+        self.save_step = args.save_step
+        if self.load_last_checkpoint is not None:
+            self.load_checkpoint(self.ckpt_name)        
+                
+        self.display_step = args.display_step
+        
+        # will store training-related information
+        self.trainstats_gather_step = args.trainstats_gather_step
+        self.trainstats_dir = args.trainstats_dir
+        if not os.path.isdir(self.trainstats_dir):
+            os.mkdir(self.trainstats_dir)        
+        self.trainstats_fname = '{}_nlatent={}_betanorm={}_{}'.format(self.model.lower(), self.n_latent, self.beta_norm, args.dataset.lower())
+        self.gather = DataGather(filename = os.path.join(self.trainstats_dir, self.trainstats_fname))
+        
+        
+        
+    def train(self):
+        
+        pbar = tqdm(total=self.max_iter)
+        pbar.update(self.global_iter)
+        
+        out = False        
+
+        running_loss_terminal_display = 0.0 # running loss for the trainstats (gathered and pickeled)
+
+        running_loss_trainstats = 0.0 # running loss for the trainstats (gathered and pickeled)
+        """ /begin of non-generic part (might need to be adapted for different models / data)"""
+        running_recon_loss_trainstats = 0.0
+        running_total_kld = 0.0
+        running_dim_wise_kld = 0.0
+        running_mean_kld = 0.0
+        
+        while not out:
+            for [samples,latents] in self.train_loader: # not sure how long the train_loader spits out data (possibly infinite?)
+                
+                self.global_iter += 1
+                pbar.update(1)
+                
+                self.net.zero_grad()               
+               
+                # get current batch and push to device
+                img_batch, _ = samples.to(self.device), latents.to(self.device)
+                
+                # in VAE, input = output/target
+                if self.modeltype == 'staticVAE':
+                    input_batch = img_batch
+                    output_batch = img_batch
+
+                                    
+                predicted_batch, mu, logvar = self.net(input_batch)
+                
+                recon_loss = self.reconstruction_loss(x = output_batch, x_recon = predicted_batch)
+                total_kld, dimension_wise_kld, mean_kld = self.kl_divergence(mu, logvar)
+                
+                actLoss = self.loss(recon_loss=recon_loss, total_kld=total_kld, beta = self.beta)
+                
+                actLoss.backward()
+                self.optim.step()                
+
+                running_loss_terminal_display += actLoss.item()
+                
+                running_loss_trainstats += actLoss.item()
+                running_recon_loss_trainstats += recon_loss.item()
+                running_total_kld += total_kld.item()
+                running_dim_wise_kld += dimension_wise_kld.cpu().detach().numpy()
+                running_mean_kld += mean_kld.item()
+                
+                # update gather object with training information
+                if self.global_iter % self.trainstats_gather_step == 0:
+                    running_loss_trainstats = running_loss_trainstats / self.trainstats_gather_step
+                    running_recon_loss_trainstats = running_recon_loss_trainstats / self.trainstats_gather_step
+                    running_total_kld = running_total_kld / self.trainstats_gather_step
+                    running_dim_wise_kld = running_dim_wise_kld / self.trainstats_gather_step
+                    running_mean_kld = running_mean_kld / self.trainstats_gather_step
+                    self.gather.insert(iter=self.global_iter,
+                                       total_loss=running_loss_trainstats,
+                                       target = output_batch[0].detach().cpu().numpy(),
+                                       reconstructed = predicted_batch[0].detach().cpu().numpy(),
+                                       recon_loss=running_recon_loss_trainstats,
+                                       total_kld = running_total_kld,
+                                       dim_wise_kld = running_dim_wise_kld,
+                                       mean_kld = running_mean_kld,
+                                       )
+                    running_loss_trainstats = 0.0
+                    running_recon_loss_trainstats = 0.0
+                    running_total_kld = 0.0
+                    running_dim_wise_kld = 0.0
+                    running_mean_kld = 0.0                    
+                
+                """ /end of non-generic part"""
+
+                if self.global_iter % self.display_step == 0:
+                    pbar.write('iter:{}, loss:{:.3e}'.format(self.global_iter, running_loss_terminal_display / self.display_step))
+                    running_loss_terminal_display = 0.0
+
+                if self.global_iter % self.save_step == 0:
+                    self.save_checkpoint(self.ckpt_name)
+                    pbar.write('Saved checkpoint(iter:{})'.format(self.global_iter))
+                    self.gather.save_data_dict()
+                
+                if self.global_iter >= self.max_iter:
+                    out = True
+                    break
+
+        pbar.write("[Training Finished]")
+        pbar.close()
+
+    
+    def save_checkpoint(self, filename, silent=True): 
+        """ saves model and optimizer state as checkpoint 
+            modified from https://github.com/1Konny/Beta-VAE/blob/master/solver.py                   
+        """
+        model_states = {'net':self.net.state_dict(),}
+        optim_states = {'optim':self.optim.state_dict(),}
+
+        
+        states = {'iter':self.global_iter,
+                  'model_states':model_states,
+                  'optim_states':optim_states}
+
+        file_path = os.path.join(self.ckpt_dir, filename)
+        with open(file_path, mode='wb+') as f:
+            torch.save(states, f)
+        if not silent:
+            print("=> saved checkpoint '{}' (iter {})".format(file_path, self.global_iter))
+    
+    def load_checkpoint(self, filename):
+        """ loads model and optimizer state from checkpoint 
+            modified from https://github.com/1Konny/Beta-VAE/blob/master/solver.py                   
+        """        
+        file_path = os.path.join(self.ckpt_dir, filename)
+        if os.path.isfile(file_path):
+            checkpoint = torch.load(file_path)
+            self.global_iter = checkpoint['iter']
+            self.net.load_state_dict(checkpoint['model_states']['net'])
+            self.optim.load_state_dict(checkpoint['optim_states']['optim'])
+            print("=> loaded checkpoint '{} (iter {})'".format(file_path, self.global_iter))
+        else:
+            print("=> no checkpoint found at '{}'".format(file_path))
+
