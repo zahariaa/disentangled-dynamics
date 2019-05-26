2abc8aaa7ffb45911668c3ef9a2e7f331d76cfc9
Andrew Zaharia <az2522@columbia.edu>
2019-05-26 13:08:53 -0400

    forgot to change kernel size

--- src/dynamic_bVAE/models.py
+++ src/dynamic_bVAE/models.py
@@ -233,11 +233,11 @@ class dynamicVAE32(nn.Module):
         self.n_frames = n_frames #=T
 
         # encoder
-        self.conv1 = nn.Conv2d(in_channels = img_channels, out_channels = 32, kernel_size = 3, stride = 2, padding = 1)     # B, 32, 16, 16, T
-        self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3, stride = 2, padding = 1)               # B, 32, 8, 8, T
-        self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 3, stride = 2, padding = 1)               # B, 64, 4, 4, T
-        self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 2, padding = 1)               # B, 64, 2, 2, T
-        self.conv5 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 2, padding = 1)               # B, 64, 1, 1, T
+        self.conv1 = nn.Conv2d(in_channels = img_channels, out_channels = 32, kernel_size = 4, stride = 2, padding = 1)     # B, 32, 16, 16, T
+        self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 4, stride = 2, padding = 1)               # B, 32, 8, 8, T
+        self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 4, stride = 2, padding = 1)               # B, 64, 4, 4, T
+        self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 4, stride = 2, padding = 1)               # B, 64, 2, 2, T
+        self.conv5 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 4, stride = 2, padding = 1)               # B, 64, 1, 1, T
 
         # Construct mu_t vector and D_t and B_t matrices which comprise mean and the inverse covariance
 # they must be constructed separately, so there is inappropriate cross-talk across time
